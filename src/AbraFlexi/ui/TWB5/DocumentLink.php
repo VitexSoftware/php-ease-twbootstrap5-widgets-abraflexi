0507e6393a1f70885cb4b43f2e87e9e10374bfb0
CyberVitexus <info@vitexsoftware.cz>
2025-06-26 11:38:11 +0200

    Add new UI components and tests for TWB5 integration
    - Implement EmbedResponsiveHTML and EmbedResponsivePDF classes for embedding HTML and PDF documents responsively.
    - Create RecordChooser and RecordSelector classes for selecting records with Selectize.js integration.
    - Introduce RecordTypeSelect for selecting record types from AbraFlexi.
    - Develop SearchBox for enhanced search functionality with datalist support.
    - Add StatusInfoBox for displaying connection status and related information.
    - Implement unit tests for all new components to ensure functionality and reliability.
    - Enhance existing tests for better coverage and validation of UI components.

---
+++ src/AbraFlexi/ui/TWB5/DocumentLink.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the BricksForAbraFlexi package
+ *
+ * https://github.com/VitexSoftware/php-abraflexi-bricks
+ *
+ * (c) Vítězslav Dvořák <http://vitexsoftware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AbraFlexi\ui\TWB5;
+
+/**
+ * Description of DocumentLink.
+ *
+ * @author Vítězslav Dvořák <info@vitexsoftware.cz>
+ */
+class DocumentLink extends \Ease\Html\ATag
+{
+    /**
+     * A Href to document in AbraFlexi web interface.
+     */
+    public function __construct(\AbraFlexi\Document $engine, string $idCode = '', string $format = '')
+    {
+        if ($idCode) {
+            $engine->setMyKey($idCode);
+        }
+
+        parent::__construct($engine->getApiUrl($format), \AbraFlexi\Functions::uncode((string) $engine->getRecordIdent()));
+    }
+}
---
+++ src/AbraFlexi/ui/TWB5/EmbedResponsive.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the BricksForAbraFlexi package
+ *
+ * https://github.com/VitexSoftware/php-abraflexi-bricks
+ *
+ * (c) Vítězslav Dvořák <http://vitexsoftware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AbraFlexi\ui\TWB5;
+
+/**
+ * Description of EmbedResponsive.
+ *
+ * @author vitex
+ */
+class EmbedResponsive extends \Ease\Html\DivTag
+{
+    public function finalize(): void
+    {
+        $this->addCSS(<<<'EOD'
+
+.embed-responsive {
+    position: relative;
+    display: block;
+    height: 0;
+    padding: 0;
+    overflow: hidden;
+}
+
+EOD);
+        parent::finalize();
+    }
+}
